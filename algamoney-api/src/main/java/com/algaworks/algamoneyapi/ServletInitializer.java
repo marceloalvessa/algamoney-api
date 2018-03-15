f7a3e192d2375a03ed2c15e11cfff52436ca8e03
marcelo <marcelo@marcelo-Inspiron-5448>
2018-03-15 00:39:45 -0300

    Commit inicial

---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.algaworks.algamoneyapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AlgamoneyApiApplication.class);
+	}
+
+}
