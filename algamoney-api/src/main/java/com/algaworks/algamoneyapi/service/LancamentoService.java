f04d55522950ee7583ede4b69d23b23be22dc878
marceloalvessa <marceloalvessa@gmail.com>
2019-06-04 18:19:08 -0300

    Atualizacao de lancamento

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/service/LancamentoService.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/service/LancamentoService.java
@@ -1,7 +1,9 @@
 package com.algaworks.algamoneyapi.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.interceptor.BeanFactoryTransactionAttributeSourceAdvisor;
 
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.model.Pessoa;
@@ -24,6 +26,38 @@ public class LancamentoService {
 			throw new PessoaInexistenteOuInativaException();
 		}
 		
-		return lancamentoRepository.save(lancamento);	}
+		return lancamentoRepository.save(lancamento);	
+	}
+	
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if(!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+		
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+		
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Pessoa pessoa = null;
+		if(lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		}
+		
+		if(pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
+		if(lancamentoSalvo == null) {
+			throw new IllegalArgumentException();
+		}
+		return lancamentoSalvo;
+	}
 
 }
