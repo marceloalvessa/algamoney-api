74e57839228e3350d8ae02319671f9261eb09696
marceloalves <marceloalves@ETB065802.caesb.gov.br>
2020-03-17 12:26:20 -0300

    22.2. Criando consulta para dados por categoria

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,15 +1,19 @@
 package com.algaworks.algamoneyapi.repository.lancamento;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.algaworks.algamoneyapi.dto.LancamentoEstatisticaCategoria;
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.filter.LancamentoFilter;
 import com.algaworks.algamoneyapi.repository.projection.ResumoLancamento;
 
 public interface LancamentoRepositoryQuery {
+	
+	public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
 	
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
