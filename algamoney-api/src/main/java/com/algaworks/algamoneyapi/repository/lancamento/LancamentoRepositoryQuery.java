479c8a474bc93fcaa43b3f5834496fc38eb6f970
marceloalves <marceloalves@ETB065802.caesb.gov.br>
2020-03-17 16:38:13 -0300

    22.4. Criando consulta para dados por dia

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.algaworks.algamoneyapi.dto.LancamentoEstatisticaCategoria;
+import com.algaworks.algamoneyapi.dto.LancamentoEstatisticaDia;
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.filter.LancamentoFilter;
 import com.algaworks.algamoneyapi.repository.projection.ResumoLancamento;
@@ -14,6 +15,7 @@ import com.algaworks.algamoneyapi.repository.projection.ResumoLancamento;
 public interface LancamentoRepositoryQuery {
 	
 	public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
+	public List<LancamentoEstatisticaDia> porDia(LocalDate mesReferencia);
 	
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
