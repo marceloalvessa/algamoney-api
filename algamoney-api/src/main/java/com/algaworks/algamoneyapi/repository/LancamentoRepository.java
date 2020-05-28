00a88baa41194fa8e33f576294a29036bbea1c63
Marcelo Alves <marceloalvessa@gmail.com>
2020-05-27 23:22:52 -0300

    22.20. Buscando lançamentos vencidos com Spring Data JPA

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/LancamentoRepository.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/LancamentoRepository.java
@@ -1,10 +1,15 @@
 package com.algaworks.algamoneyapi.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.lancamento.LancamentoRepositoryQuery;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
+	
+	List<Lancamento> findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate data);
 
 }
